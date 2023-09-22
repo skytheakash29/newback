6bfe402dc6dbae71a6214cf761857b35941bac72
Skytheemadulislam <skytheemadulislam@gmail.com>
2023-09-22 16:37:08 +0600

    openai added

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-// const Configuration = require("openai");
-// const OpenAIApi = require("openai");
+const Configuration = require("openai");
+const OpenAIApi = require("openai");
 
 const app = express();
 app.use(cors());
@@ -10,33 +10,31 @@ app.use(express.json());
 
 app.listen(4000);
 
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY, 
-// });
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY, 
+});
 
-// const OpenAI = require("openai");
+const OpenAI = require("openai");
 
-// const openai = new OpenAI();
+const openai = new OpenAI();
 
-// async function main(prompt,ratio) {
-//   const image = await openai.images.generate({ prompt:`${prompt}`,n: 1,
-//   size: "1024x1024"});
+async function main(prompt,ratio) {
+  const image = await openai.images.generate({ prompt:`${prompt}`,n: 1,
+  size: "1024x1024"});
 
-//     return image.data[0].url
+    return image.data[0].url
 
-// }
-
-
+}
 
 
 
-// app.post("/generate", async (req, res) => {
-//     let data = await main(req.body.prompt);
-
-//   res.send({data})
-// });
 
 
+app.post("/generate", async (req, res) => {
+    let data = await main(req.body.prompt);
+
+  res.send({data})
+});
 
 
 
@@ -52,17 +50,19 @@ app.listen(4000);
 
 
 
-// // code for chat iuntergation
-// app.post("/chat",async(req,res)=>{
-//   const {message} = req.body;
-//   try{
-//     const response = await openai.chat.completions.create({
-//       model: "gpt-3.5-turbo-16k-0613",
-//       messages: [{ role: "system", content: `${message}` }],
-//   })
-//   res.json({message: response.choices[0].message.content})
 
-// }catch(e){
-//     res.send(e).status(400)
-// }
-// })
+
+// code for chat iuntergation
+app.post("/chat",async(req,res)=>{
+  const {message} = req.body;
+  try{
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo-16k-0613",
+      messages: [{ role: "system", content: `${message}` }],
+  })
+  res.json({message: response.choices[0].message.content})
+
+}catch(e){
+    res.send(e).status(400)
+}
+})
